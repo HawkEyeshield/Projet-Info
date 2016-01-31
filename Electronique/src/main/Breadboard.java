d08becb805a9f5fe607bc4d0fb441d170fc72f5b
CF <francois.charles@telecom-sudparis.eu>
2016-01-31 16:26:57 +0100

    Ajout du code existant sur lequel bosser

---
+++ Electronique/src/main/Breadboard.java
@@ -0,0 +1,82 @@
+package main;
+
+import java.util.ArrayList;
+
+/**
+ * Classe qui liste les différents composants et trouve leurs potentiel et courants
+ * @author CF
+ */
+public class Breadboard
+{
+	private ArrayList<Resistor> listr;
+	
+	private ArrayList<Generator> listg;
+	
+	public Breadboard(ArrayList<Resistor> list, ArrayList<Generator> listg)
+	{
+		this.listr=list;
+		this.listg=listg;
+	}
+	
+	/**
+	 * Etablit les potentiels
+	 */
+	public void setPotentials()
+	{
+		for (int i=0;i<this.listg.size();i++)
+		{
+			for (int j=0;j<this.listr.size();j++)
+			{
+				if (this.listr.get(j).getFirstLink() == this.listg.get(i).getFirstLink())
+				{
+					this.listr.get(j).setFirstPotential(this.listg.get(i).getFirstPotential());
+				}
+				if (this.listr.get(j).getSecondLink() == this.listg.get(i).getSecondLink())
+				{
+					this.listr.get(j).setSecondPotential(this.listg.get(i).getSecondPotential());
+				}
+				if (this.listr.get(j).getFirstLink() == this.listg.get(i).getSecondLink())
+				{
+					this.listr.get(j).setFirstPotential(this.listg.get(i).getSecondPotential());
+				}
+				if (this.listr.get(j).getSecondLink() == this.listg.get(i).getFirstLink())
+				{
+					this.listr.get(j).setSecondPotential(this.listg.get(i).getFirstPotential());
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Méthode calculant les courants après avoir trouvé les potentiels
+	 */
+	public void setCurrents()
+	{
+		for (int i=0; i<this.listr.size();i++)
+		{
+			this.listr.get(i).computeCurrent();
+		}
+	}
+	
+	public String toString()
+	{
+		String str="";
+		
+		for (int i=0; i<this.listg.size();i++)
+		{
+			str+= "Premier potentiel du générateur " + this.listg.get(i).getName() + " : ";
+			str+= this.listg.get(i).getFirstPotential() + " V \n";
+			str+= "Second potentiel du générateur  " + this.listg.get(i).getName() + " : ";
+			str+=this.listg.get(i).getSecondPotential() + " V \n";
+		}
+		for (int i=0;i<listr.size();i++)
+		{
+			str+= "Premier potentiel de la résistance " + this.listr.get(i).getName() + " : ";
+			str+= this.listr.get(i).getFirstPotential() + " V \n";
+			str+= "Second potentiel de la résistance  "  +this.listr.get(i).getName() + " : ";
+			str+= this.listr.get(i).getSecondPotential()+ " V \n";
+			str+="Courant traversant la résistance : " + this.listr.get(i).getCurrent() + " A \n";
+		}
+		return str;
+	}
+}
---
+++ Electronique/src/main/Main.java
@@ -0,0 +1,34 @@
+package main;
+
+import java.util.ArrayList;
+
+/** Classe Main du projet, boucle principale  
+ * @author CF 
+ */
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		Resistor r1 = new Resistor("r1", 1, 0,100);
+		Resistor r2 = new Resistor("r2", 1, 0,200);
+		Resistor r3 = new Resistor("r3", 2, 0,100);
+		Generator g1 = new Generator("g1", 1, 0,15,0);
+		Generator g2 = new Generator("g2", 2, 0, 10, 0);
+		
+		ArrayList<Resistor> l1 = new ArrayList<Resistor>();
+		l1.add(r1);
+		l1.add(r2);
+		l1.add(r3);
+		
+		ArrayList<Generator> l2 = new ArrayList<Generator>();
+		l2.add(g1);
+		l2.add(g2);
+		
+		Breadboard b = new Breadboard(l1, l2);
+		b.setPotentials();
+		b.setCurrents();
+		System.out.println(b);
+	}
+
+}
