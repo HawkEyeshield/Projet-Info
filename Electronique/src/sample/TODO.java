46e58dcf495ddb71c9e7e596e21024c3160806db
tanguy <tanguy.kerdoncuff@telecom-sudparis.eu>
2016-03-17 15:35:22 +0100

    Ajout de la partie graphique

---
+++ Electronique/src/sample/TODO.java
@@ -0,0 +1,22 @@
+package sample;
+
+/**
+ * Created by tanguy on 09/03/16.
+ */
+public class TODO {
+    //Classe débile pour les TODO
+    //TODO
+    /*
+    trouver toutes les images et les mettres dans le dossier
+    redimensionner toutes les images pour avoir le moins de blanc autour (image circulaire ? pk pas !)
+    mettre les images dans le code
+
+    ajuster les dimensions des différentes parties
+
+    réflechir a toutes les boutons, car il en y plus que prévu
+
+    mettre en place les liens
+
+
+     */
+}
