200964e574d46f509f76e2f865e4c67f43fc4375
CF <francois.charles@telecom-sudparis.eu>
2016-03-20 22:22:08 +0100

    modifs des packages

---
+++ Electronique/src/graphics/AddLink.java
@@ -0,0 +1,32 @@
+package graphics;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Line;
+
+
+/**
+ * @author Tanguy
+ * Permet de créer des liens entre les composants
+ */
+public class AddLink 
+{
+
+    private class link
+    {
+        ImageView image1;
+        char zone1;
+        ImageView image2;
+        char zone2;
+        Line line1; //Line 1 est relie a l'image 1
+        Line line2; //Line 2 est relié a l'image 2
+
+        public void Link(ImageView image1,ImageView image2, Line line1, Line line2, char zone1, char zone2) {
+            this.image1 = image1;
+            this.image2 = image2;
+            this.line1 = line1;
+            this.line2 = line2;
+            this.zone1 = zone1;
+            this.zone2 = zone2;
+        }
+    }
+}
---
+++ Electronique/src/graphics/Main.java
@@ -0,0 +1,46 @@
+package graphics;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class Main extends Application 
+{
+
+    @FXML
+    private ImageView imageView;
+
+    /**
+     * start permet de lancer l'interface graphique
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        URL location = getClass().getResource("sample.fxml");
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(location);
+        fxmlLoader.setBuilderFactory(new JavaFXBuilderFactory());
+
+        Parent root = (Parent) fxmlLoader.load(location.openStream());
+
+        primaryStage.setTitle("Circuit Electrique");
+        primaryStage.setScene(new Scene(root, 1100, 690));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) 
+    {
+        launch(args);
+    }
+}
