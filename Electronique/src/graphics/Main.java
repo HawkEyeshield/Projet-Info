d6cfe1a942e085224abdc9b5dc89ec4605a12a2e
CF <francois.charles@telecom-sudparis.eu>
2016-05-22 16:26:13 +0200

    Le projet s'appelle officiellement Kazento

--- Electronique/src/graphics/Main.java
+++ Electronique/src/graphics/Main.java
@@ -33,7 +33,7 @@ public class Main extends Application
 
         Parent root = (Parent) fxmlLoader.load(location.openStream());
 
-        primaryStage.setTitle("Circuit Electrique"); //permet de donner un titre à la fenêtre
+        primaryStage.setTitle("Kazento - Simulateur de circuits électriques"); //permet de donner un titre à la fenêtre
         primaryStage.setScene(new Scene(root, 1100, 690)); //définit la taille
         primaryStage.show(); //affiche l'interface
     }
